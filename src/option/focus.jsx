b94479efbfa2825d820e9fa21bb1e7ec1c30720e
Kenshin <fxblog@gmail.com>
2016-12-26 19:02:24 +0800

    Add same focus option style.

--- src/option/focus.jsx
+++ src/option/focus.jsx
@@ -8,12 +8,18 @@ export default class FocusOpt extends React.Component {
         $( ".ks-simpread-bg" ).css( "background-color", $(event.target).css( "background-color" ));
     }
 
+    changeShortcuts () {
+        console.log(event.key,event.keyCode)
+        //$( this.refs.shortcuts ).text( event.key );
+        //this.refs.shortcuts.value = event.key;
+    }
+
     render() {
         return (
             <div className="ks-simpread-option-focus">
                 <div className="ks-simpread-option-focus-container">
                     <span>主题色：</span>
-                    <ul className="ks-simpread-option-focus-theme"  onClick={ ()=> this.changeBgColor() }>
+                    <ul className="ks-simpread-option-focus-theme" onClick={ ()=> this.changeBgColor() }>
                         <li className="ks-simpread-option-focus-theme-item ks-simpread-option-focus-theme-item-active"></li>
                         <li className="ks-simpread-option-focus-theme-item"></li>
                         <li className="ks-simpread-option-focus-theme-item"></li>
@@ -27,8 +33,13 @@ export default class FocusOpt extends React.Component {
                 <div className="ks-simpread-option-focus-container">
                     <span>透明度：</span>
                     <div className="ks-simpread-option-focus-opacity">
-                        <input type="range" min="0" max="100" step="10" value="90">
-                        </input>
+                        <input type="range" min="0" max="100" step="10" value="90"></input>
+                    </div>
+                </div>
+                <div className="ks-simpread-option-focus-container">
+                    <span>快捷键：</span>
+                    <div className="ks-simpread-option-focus-shortcuts">
+                        <input ref="shortcuts" type="text" value="A S" onKeyPress={ ()=> this.changeShortcuts()  }></input>
                     </div>
                 </div>
             </div>
