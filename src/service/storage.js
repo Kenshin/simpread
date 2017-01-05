c5ce6afb2d3d416fd1435c57eb126d8b681a4000
Kenshin <fxblog@gmail.com>
2017-01-05 16:25:25 +0800

    Optimize storage.js init data structure.

--- src/service/storage.js
+++ src/service/storage.js
@@ -14,7 +14,7 @@ const mode = {
         read   : "read",
         option : "option",
     },
-    name = "simpread";
+    name = "simpread_test";
 
 let site  = {
         exclude   : [],
@@ -31,7 +31,7 @@ let site  = {
         bgcolor   : "rgba( 235, 235, 235, 0.9 )",
         opacity   : 90,
         shortcuts : "A S",
-        sites     : [[ "url", site ]],
+        sites     : [[ "<url>", site ]],
     },
     simpread = {
         focus,
