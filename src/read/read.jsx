263ce18c9eb15eee8be6a22570fd89064637b642
Kenshin <fxblog@gmail.com>
2017-01-10 18:32:00 +0800

    Add read.formatHtml() method remove style property.

--- src/read/read.jsx
+++ src/read/read.jsx
@@ -36,7 +36,7 @@ class Read extends React.Component {
             <sr-read class="sr-rd-font">
                 <sr-rd-title>{ this.props.wrapper.title }</sr-rd-title>
                 <sr-rd-desc>{ this.props.wrapper.desc }</sr-rd-desc>
-                <sr-rd-content dangerouslySetInnerHTML={{__html: this.props.wrapper.include }} ></sr-rd-content>
+                <sr-rd-content dangerouslySetInnerHTML={{__html: formatHtml(this.props.wrapper.include) }} ></sr-rd-content>
             </sr-read>
         )
     }
@@ -52,18 +52,32 @@ function Render() {
     ReactDOM.render( <Read read={ storage.current } wrapper={ wrap(storage.current.site) } />, getReadRoot() );
 }
 
+/**
+ * Wrap storage.current.site object
+ * 
+ * @param  {object} storage.current.site object
+ * @return {object} wrapper object
+ */
 function wrap( site ) {
     const wrapper  = Clone( site ),
           title   = util.selector( site.title   ),
           desc    = util.selector( site.desc    ),
           include = util.selector( site.include );
-
     wrapper.title   = $root.find( title ).text().trim();
     wrapper.desc    = $root.find( desc ).text().trim();
     wrapper.include = $root.find( include ).html();
     //util.excludeStyle( $root, site.exclude, "hide" );
-
     return wrapper;
+}
+
+/**
+ * Format html
+ * 
+ * @param  {string} html string
+ * @return {string} format html string
+ */
+function formatHtml( html ) {
+    return html.trim().toLowerCase().replace( / style="[a-z0-9-_: ;#.]+"/g, "" );
 }
 
 /**
