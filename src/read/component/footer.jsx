edef3f478390dd7a27437f690ecba018791ba536
Kenshin <fxblog@gmail.com>
2017-01-19 12:11:09 +0800

    Update copywrite.

--- src/read/component/footer.jsx
+++ src/read/component/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <sr-rd-footer>
             <sr-rd-footer-end>全文完</sr-rd-footer-end>
-            <p>本文由 简悦 <a href="http://ksria.com/simpread" target="_blank">SimpRead</a> 转码</p>
+            <p>本文由 简悦 <a href="http://ksria.com/simpread" target="_blank">SimpRead</a> 转码，对当前页面做了优化，以提升阅读体验。</p>
         </sr-rd-footer>
     )
 }
