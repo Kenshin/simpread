856c4699a3062cdf5c5fb4931801ff0a126295fe
Kenshin <fxblog@gmail.com>
2017-02-16 15:47:04 +0800

    Remove old source.

--- src/option/focus.jsx
+++ src/option/focus.jsx
@@ -1,8 +1,5 @@
 console.log( "===== simpread option focus mode load =====" )
 
-//import Notify    from 'notify';
-//import * as util from 'util';
-
 import ThemeSel  from 'themesel';
 import Shortcuts from 'shortcuts';
 import Include   from 'include';
@@ -56,18 +53,12 @@ export default class FocusOpt extends React.Component {
     }
 
     changeExclude( value ) {
-        //this.props.option.site.exclude = getExclude( event.target.value );
         this.props.option.site.exclude = value;
         console.log( "this.props.option.site.exclude = ", this.props.option.site.exclude )
     }
 
     componentDidMount() {
         this.refs.opacity.value   = this.props.option.opacity;
-        //this.refs.exclude.value   = this.props.option.site.exclude.join( "\n") ;
-    }
-
-    constructor( props ) {
-        super( props );
     }
 
     render() {
@@ -138,41 +129,3 @@ function getColor( value ) {
         return null;
     }
 }
-
-/**
- * Get exclude tags
- * 
- * @param  {string} input exclude html tag, e.g.:
-    <div class="article fmt article__content">
-    <h1>
-    <div class="col-xs-12 col-md-9 main ">
-    <img id="icon4weChat" style="height: 0;width: 0;">
-    <div class="wsx_fade" style="pointer-events: none;"></div>
-    sdasdfas
-    
-
-    <div class="ks-simpread-bg">
- *
- * @return {array} verify success htmls
-    <div class="article fmt article__content">
-    <h3 id="articleHeader1">原著序</h3>
-    <div class="col-xs-12 col-md-9 main ">
-    <img id="icon4weChat" style="height: 0;width: 0;">
-    <div class="wsx_fade" style="pointer-events: none;"></div>
-    <div class="ks-simpread-bg">
- * 
- */
-/*
-function getExclude( htmls ) {
-    let [ list, obj ]  = [[], null ];
-    const arr = htmls.trim().split( "\n" );
-    for( let value of arr ) {
-        if ( util.verifyHtml( value.trim() )[0] > 0 ) {
-            list.push( value.trim() );
-        } else {
-            //new Notify().Render( 2, `当前输入【 ${value} 】错误，请重新输入。` );
-        }
-    }
-    return list;
-}
-*/
--- src/option/read.jsx
+++ src/option/read.jsx
@@ -1,6 +1,5 @@
 console.log( "===== simpread option read mode load =====" )
 
-//import Notify    from 'notify';
 import th        from 'theme';
 
 import ThemeSel  from 'themesel';
@@ -27,17 +26,8 @@ export default class ReadOpt extends React.Component {
     }
 
     changeExclude( value ) {
-        //this.props.option.site.exclude = getExclude( event.target.value );
         this.props.option.site.exclude = value;
         console.log( "this.props.option.site.exclude = ", this.props.option.site.exclude )
-    }
-
-    /*componentDidMount() {
-        this.refs.exclude.value   = this.props.option.site.exclude.join( "\n") ;
-    }*/
-
-    constructor( props ) {
-        super( props );
     }
 
     render() {
@@ -69,64 +59,3 @@ export default class ReadOpt extends React.Component {
         )
     }
 }
-
-/**
- * Get exclude tags
- * 
- * @param  {string} input exclude html tag, e.g.:
-    <div class="article fmt article__content">
-    <h1>
-    <div class="col-xs-12 col-md-9 main ">
-    <img id="icon4weChat" style="height: 0;width: 0;">
-    <div class="wsx_fade" style="pointer-events: none;"></div>
-    sdasdfas
-    
-
-    <div class="ks-simpread-bg">
- *
- * @return {array} verify success htmls
-    <div class="article fmt article__content">
-    <h3 id="articleHeader1">原著序</h3>
-    <div class="col-xs-12 col-md-9 main ">
-    <img id="icon4weChat" style="height: 0;width: 0;">
-    <div class="wsx_fade" style="pointer-events: none;"></div>
-    <div class="ks-simpread-bg">
- * 
- */
-/*
-function getExclude( htmls ) {
-    let [ list, obj ]  = [[], null ];
-    const arr = htmls.trim().split( "\n" );
-    for( let value of arr ) {
-        if ( verifyHtml( value.trim() ) > 0 ) {
-            list.push( value.trim() );
-        } else {
-            //new Notify().Render( 2, `当前输入【 ${value} 】错误，请重新输入。` );
-        }
-    }
-    return list;
-}
-*/
-
-/**
- * Verify html
- * 
- * @param  {string} input include html tag, e.g.:
-    <div class="article fmt article__content">
- *
- * @return {int} -1: fail； 0: emputy html; 1:success
- * 
- */
-/*
-function verifyHtml( html ) {
-    if ( html == "" ) return 0;
-    const item = html.match( / (class|id)=("|')[\w-_]+/ig );
-    if ( item && item.length > 0 ) {
-        //[tag, name] = item[0].trim().replace( /'|"/ig, "" ).split( "=" );
-        //return { tag, name };
-        return 1;
-    } else {
-        return -1;
-    }
-}
-*/
