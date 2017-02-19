a15dd1b871675c02af97ae8f00dbad603580d971
Kenshin <fxblog@gmail.com>
2017-02-19 15:28:25 +0800

    Add css in js logic.

--- src/comp/textfield.jsx
+++ src/comp/textfield.jsx
@@ -1,9 +1,162 @@
 console.log( "==== simpread component: TextField ====" )
 
 let $target, $float, $state, $border, $error,
-    element;
+    element, styles;
 
-const [ MIN_ROWS, steps ] = [ 3, 24 ];
+const [ MIN_ROWS, steps ] = [ 3, 24 ],
+      cssinjs = ( props )=>{
+
+    const color = 'rgba(51, 51, 51, .87)',
+          styles = {
+            root: {
+                font: '300 16px/1.8 PingFang SC, Lantinghei SC, Microsoft Yahei, Hiragino Sans GB, Microsoft Sans Serif, WenQuanYi Micro Hei, sans',
+
+                display: 'inline-block',
+                position: 'relative',
+
+                width: '100%',
+                height: '72px',
+
+                marginBottom: '8px',
+            },
+
+            input: {
+                color,
+                backgroundColor: 'transparent',
+
+                height: '20px',
+                width: '100%',
+
+                marginTop: '8px',
+                padding: 0,
+
+                border: 'none',
+                outline: 'none',
+
+                boxShadow: 'none',
+                boxSizing: 'content-box',
+                transition: 'all 0.3s',
+            },
+
+            textarea : {
+                position: 'relative',
+
+                color,
+                backgroundColor: 'transparent',
+
+                height: '60px',
+                width: '100%',
+
+                marginTop: '8px',
+                padding: 0,
+
+                lineHeight: 1.5,
+
+                cursor: 'inherit',
+
+                border: 'none',
+                outline: 'none',
+                resize: 'none',
+
+                boxSizing: 'border-box',
+                WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+                WebkitAppearance: 'textfield',
+            },
+
+            float : {
+                display: 'block',
+                position: 'absolute',
+
+                top: '12px',
+
+                fontSize: '16px',
+                fontWeight: 400,
+                color: 'rgba(0, 0, 0, 0.298039)',
+
+                userSelect: 'none',
+                pointerEvents: 'none',
+
+                transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+                transform: 'scale(1) translate( 0px, 0px )',
+                transformOrigin: 'left top 0px',
+            },
+
+            border : {
+                display: 'block',
+
+                margin: '8px 0 0 0',
+                width: '100%',
+
+                borderTop: 'none rgb(224, 224, 224)',
+                borderLeft: 'none rgb(224, 224, 224)',
+                borderRight: 'none rgb(224, 224, 224)',
+                borderBottom: '1px solid rgb(224, 224, 224)',
+                boxSizing: 'content-box',
+            },
+
+            state : {
+                display: 'block',
+                position: 'absolute',
+                bottom: '34px',
+
+                margin: 0,
+                width: '100%',
+
+                borderTop: 'none rgba(0, 137, 123, .8)',
+                borderLeft: 'none rgba(0, 137, 123, .8)',
+                borderRight: 'none rgba(0, 137, 123, .8)',
+                borderBottom: '2px solid rgba(0, 137, 123, .8)',
+                boxSizing: 'content-box',
+
+                transform: 'scaleX(0)',
+                transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+            },
+
+            error : {
+                display: 'block',
+
+                marginTop: '8px',
+
+                fontSize: '14px',
+                fontWeight: 'bold',
+                lineHeight: '12px',
+
+                userSelect: 'none',
+
+                color: 'rgb(244, 67, 54)',
+                transform: 'scale(0.75) translate( -80px, 0 )',
+            }
+        };
+
+        const classes = {
+            display : 'none',
+            floated : {
+                color: '#00897B',
+
+                top: '-8px',
+
+                fontSize: '14px',
+                fontWeight: 'bold',
+
+                transform: 'scale(0.75) translate( 0px, -8px )',
+            },
+            focus : {
+                transform: 'scaleX(1)',
+            }
+        }
+
+        if ( props.floatingtext == "" ) styles.float.display = classes.display;
+        if ( props.placeholder != ""  ) styles.float = { ...styles.float, ...classes.floated }
+        if ( props.multi && ( props.rows > MIN_ROWS )) {
+            const rows      = props.rows - MIN_ROWS,
+                  height    = Number.parseInt(styles.textarea.height),
+                  parheight = Number.parseInt(styles.root.height);
+             styles.textarea.height = `${height + rows * steps}px`;
+             styles.root.height     = `${parheight + rows * steps}px`;
+        }
+
+    return styles;
+}
 
 export default class TextField extends React.Component {
 
@@ -25,8 +178,8 @@ export default class TextField extends React.Component {
 
     changeFocus() {
         setjQueryObj( this.refs );
-        $float.addClass( "text-field-floated" );
-        $state.addClass( "text-field-state-focus" );
+        //$float.addClass( "text-field-floated" );
+        //$state.addClass( "text-field-state-focus" );
         $target.css( "font-size", "13px" );
     }
 
@@ -34,9 +187,9 @@ export default class TextField extends React.Component {
         setjQueryObj( this.refs );
         const val = $target.val();
         if ( val == "" && $target.attr( "placeholder" ) == "" ) {
-            $float.removeAttr( "class" );
+            //$float.removeAttr( "class" );
         }
-        $state.removeAttr( "class" );
+        //$state.removeAttr( "class" );
         if ( val == "" ) $target.css( "font-size", "16px" );
     }
 
@@ -57,25 +210,27 @@ export default class TextField extends React.Component {
     componentDidMount() {
         setjQueryObj( this.refs );
 
-        if ( this.props.floatingtext == "" ) $float.hide();
-        if ( $target.attr( "placeholder" ) != "" ) $float.addClass( "text-field-floated" );
-        if ( this.props.multi && ( this.props.rows > MIN_ROWS )) {
+        //if ( this.props.floatingtext == "" ) $float.hide();
+        //if ( $target.attr( "placeholder" ) != "" ) $float.addClass( "text-field-floated" );
+        /*if ( this.props.multi && ( this.props.rows > MIN_ROWS )) {
             const rows   = this.props.rows - MIN_ROWS,
                   height = $target.height(),
                   parheight = $target.parent().height();
             $target.height( height + rows * steps );
             $target.parent().height( parheight + rows * steps );
-        }
+        }*/
     }
 
      constructor( props ) {
         super( props );
+        styles = cssinjs( this.props );
     }
 
     render() {
 
         element = this.props.multi ? (
             <textarea ref="target" 
+                       style={ styles.textarea }
                        placeholder={ this.props.placeholder }
                        onFocus  ={ ()=>this.changeFocus() }
                        onBlur   ={ ()=>this.changeBlur() }
@@ -83,6 +238,7 @@ export default class TextField extends React.Component {
             />
         ) : (
             <input ref="target" 
+                       style={ styles.input }
                        type="text" 
                        placeholder={ this.props.placeholder }
                        onFocus={ ()=>this.changeFocus() }
@@ -91,14 +247,14 @@ export default class TextField extends React.Component {
         );
 
         return (
-            <text-field>
-                <text-field-float ref="float">{this.props.floatingtext}</text-field-float>
+            <text-field style={ styles.root }>
+                <text-field-float ref="float" style={ styles.float }>{this.props.floatingtext}</text-field-float>
                 { element }
                 <div>
-                    <text-field-border ref="border"/>
-                    <text-field-state ref="state"/>
+                    <text-field-border ref="border" style={ styles.border }/>
+                    <text-field-state ref="state" style={ styles.state }/>
                 </div>
-                <text-field-error ref="error" dangerouslySetInnerHTML={{__html: this.props.errortext }}></text-field-error>
+                <text-field-error ref="error" style={ styles.error } dangerouslySetInnerHTML={{__html: this.props.errortext }}></text-field-error>
             </text-field>
         )
     }
