4887e5ffc2ad8d3ae48466246e61d6544ede9aa7
Kenshin <fxblog@gmail.com>
2017-03-01 14:30:29 +0800

    Modify tooltip logic.

--- src/comp/tooltip.jsx
+++ src/comp/tooltip.jsx
@@ -5,15 +5,15 @@ const cssinjs = () => {
     const styles = {
 
         root: {
-            position: 'absolute',
+            position: 'relative',
 
             padding: '0px 16px',
 
             width: '105px',
             height: '32px',
 
-            top: '-10000px',
-            left: '-28px',
+            //top: '-10000px',
+            //left: '-28px',
 
             zIndex: 3000,
 
@@ -24,13 +24,18 @@ const cssinjs = () => {
             color: 'rgb(255, 255, 255)',
 
             overflow: 'hidden',
-            opacity: 1,
+            //opacity: 0,
 
             userSelect: 'none',
 
             transition: 'top 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, transform 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, opacity 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
             borderRadius: '2px',
             boxSizing: 'border-box',
+
+            top: '36PX',
+            left: '-28.5px',
+            opacity: 0.9,
+            transfrom: 'translate( 0px, 24px )',
         },
 
         root_focus: {
@@ -43,21 +48,27 @@ const cssinjs = () => {
         text: {
             position: 'absolute',
 
-            width: 0,
-            height: 0,
+            //width: 0,
+            //height: 0,
 
             left: '50%',
             top: 0,
 
-            backgroundColor: 'transparent',
+            //backgroundColor: 'transparent',
 
             borderRadius: '50%',
             transform: 'translate(-50%, -50%)',
-            transition: 'width 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, height 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, backgroundColor 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+            //transition: 'width 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, height 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, backgroundColor 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+
+            width: '123px',
+            height: '123px',
+
+            backgroundColor: 'rgb(97, 97, 97)',
+
+            transition: 'width 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, height 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, backgroundColor 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
         },
 
         text_focus: {
-
             width: '123px',
             height: '123px',
 
@@ -78,48 +89,51 @@ const cssinjs = () => {
 class ToolTip extends React.Component {
 
     static defaultProps = {
+        text   : "",
         target : "",
     }
 
     static propTypes = {
+        text  : React.PropTypes.string,
         target: React.PropTypes.string,
     }
     
     state = {
-        id  : Math.round(+new Date()),
-        text: "",
-    }
-
-    onMouseOver() {
-        $( this.refs.root ).css({ ...style.root, ...style.root_focus });
-        $( this.refs.text ).css({ ...style.text, ...style.text_focus });
-        this.setState({ text: $( event.target ).attr( "tooltip" ) });
-    }
-
-    componentDidMount() {
-        $( "html" ).on( "mouseover", "[tooltip]", this.onMouseOver.bind( this ) );
+        id : Math.round(+new Date()),
     }
 
     render() {
+
+        console.log("asdfassdfasdfadsf")
 
         styles.set( this.state.id, cssinjs() );
         style = styles.get( this.state.id );
 
         return (
-            <div ref="root" style={ style.root }>
-                <div ref="text" style={ style.text }></div>
-                <span style={ style.span }>{ this.state.text }</span>
-            </div>
+            <sr-tooltip>
+                <div ref="root" style={{ ...style.root, ...style.root_focus }}>
+                    <div ref="text" style={{ ...style.text, ...style.text_focus }}></div>
+                    <span style={ style.span }>{ this.props.text }</span>
+                </div>
+            </sr-tooltip>
         )
     }
 
 }
 
-function Render( target ) {
-    const $target = $(target);
-    $target.append( "<sr-tooltip></sr-tooltip>" );
-    const $root = $target.find( "sr-tooltip" );
-    ReactDOM.render( <ToolTip />, $root[0] );
+function Render() {
+    $( "html" ).on( "mouseenter", "[tooltip]", ()=>{
+        const $target = $( event.target );
+        console.log( $target.find( "sr-tooltip" ).length )
+        
+        if ( $target.find( "sr-tooltip" ).length == 0 ) {
+            ReactDOM.render( <ToolTip text={ $target.attr( "tooltip" ) } />, event.target );
+            $target.one( "mouseleave", ()=>{
+                console.log("sfasdfasf", $( event.target ))
+                $( event.target ).remove();
+            });
+        }
+    });
 }
 
 export { Render };
