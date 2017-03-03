321fa0d697327dc521a8e1491dec0d7d0aa89eb0
Kenshin <fxblog@gmail.com>
2017-03-03 12:12:21 +0800

    Remove old source.

--- src/comp/tooltip.jsx
+++ src/comp/tooltip.jsx
@@ -7,73 +7,6 @@ let started = false, timeout, $target, $back, style, styles = new Map();
 const cssinjs = () => {
     const styles = {
 
-        /*root: {
-            position: 'relative',
-
-            padding: '0px 16px',
-
-            width: '105px',
-            height: '32px',
-
-            top: '-10000px',
-            left: '-28px',
-
-            zIndex: 3000,
-
-            fontSize: '14px',
-            lineHeight: '32px',
-            textAlign: 'center',
-
-            color: 'rgb(255, 255, 255)',
-
-            overflow: 'hidden',
-            opacity: 0,
-
-            userSelect: 'none',
-
-            transition: 'top 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, transform 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, opacity 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-            borderRadius: '2px',
-            boxSizing: 'border-box',
-        },
-
-        root_focus: {
-            top: '36PX',
-            left: '-28.5px',
-            opacity: 0.9,
-            transform: 'translate( 0px, 0px )',
-            transition: 'top 0ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, transform 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, opacity 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        },
-
-        text: {
-            position: 'absolute',
-
-            width: 0,
-            height: 0,
-
-            left: '50%',
-            top: 0,
-
-            backgroundColor: 'transparent',
-
-            borderRadius: '50%',
-            transform: 'translate(-50%, -50%)',
-            transition: 'width 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, height 0ms cubic-bezier(0.23, 1, 0.32, 1) 450ms, backgroundColor 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        },
-
-        text_focus: {
-            width: '123px',
-            height: '123px',
-
-            backgroundColor: 'rgb(97, 97, 97)',
-
-            transition: 'width 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, height 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, backgroundColor 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        },
-
-        span: {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-        },*/
-
         root : {
             font: '300 14px/1.8 PingFang SC, Lantinghei SC, Microsoft Yahei, Hiragino Sans GB, Microsoft Sans Serif, WenQuanYi Micro Hei, sans',
 
@@ -257,17 +190,6 @@ function Render( root ) {
         ReactDOM.render( <ToolTip text={ text } position={ position} delay={ delay } item={ $item } />, getTooltipRoot( $root ) );
         //$item.tooltip({ position, delay, text, root });
     });
-    /*
-    $( "html" ).on( "mouseenter", "[tooltip]", ()=>{
-        const $target = $( event.target );        
-        if ( $target.find( "sr-tooltip" ).length == 0 ) {
-            ReactDOM.render( <ToolTip text={ $target.attr( "tooltip" ) } />, event.target );
-            $target.one( "mouseleave", () => {
-                $( event.currentTarget ).find( "sr-tooltip" ).remove();
-            });
-        }
-    });
-    */
 }
 
 /**
