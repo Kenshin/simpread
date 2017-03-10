9e98163d7f9789f469207fe94fc254d509190eab
Kenshin <fxblog@gmail.com>
2017-03-10 11:18:09 +0800

    Remove test code.

--- src/vender/notify/notify.js
+++ src/vender/notify/notify.js
@@ -105,10 +105,10 @@ var Notify = ( function () {
             hidden( $(this).parent() );
         },
         hidden = function( target ) {
-            /*target.addClass( "notify-hide" ).slideUp( 500, function() {
+            target.addClass( "notify-hide" ).slideUp( 500, function() {
                 target.remove();
                 if ($root.children().length === 0 ) $root.css( "z-index", 0 );
-            });*/
+            });
         },
         render = function() {
             var $target  = $( TMPL ),
