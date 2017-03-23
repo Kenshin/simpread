09c89c532bd1591678ff5096c9315ddb93dd6ff4
Kenshin <fxblog@gmail.com>
2017-03-23 18:46:33 +0800

    Remove console.

--- src/optionspage/options.js
+++ src/optionspage/options.js
@@ -6,7 +6,6 @@ import '../assets/css/options.css';
  * Add parallax scroll
  */
 $( window ).scroll( (event) => {
-    console.log( 0 - $(event.target).scrollTop(), document.body.scrollTop )
     const $target = $( event.target ),
           offset  = ( 0 - $target.scrollTop() ) / 2;
     $( ".top" ).css( "transform", `translate3d(0px, ${offset}px, 0px)` )
