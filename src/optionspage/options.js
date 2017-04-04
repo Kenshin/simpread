cb63b4727ffb9b75e25734809f113d0210ab09e8
Kenshin <fxblog@gmail.com>
2017-04-04 15:22:23 +0800

    Fix init not call storage.GetNewsites() bug.

--- src/optionspage/options.js
+++ src/optionspage/options.js
@@ -109,16 +109,15 @@ storage.Get( first => {
  * @param {bool} is first load
  */
 function firstLoad( first ) {
-    if ( window.location.hash && window.location.hash == "#firstload" && first ) {
-        storage.GetNewsites( "local", ( _, error ) => {
-            error && new Notify().Render( 0, "本地更新出现错误，请选择手动点击 同步配置列表" );
-        });
+    first && storage.GetNewsites( "local", ( _, error ) => {
+        error && new Notify().Render( 0, "本地更新出现错误，请选择手动点击 同步配置列表" );
+    });
+    window.location.hash && window.location.hash == "#firstload" && first &&
         storage.Sync( "get", success => {
             success && ReactDOM.unmountComponentAtNode( $( ".tabscontainer" )[0] );
             success && new Notify().Render( 0, "数据恢复成功！" );
             success && mainRender( tabsItemID );
-        });
-    }
+    });
 }
 
 /**
