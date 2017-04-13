0b2f0553c0ce7b2fc7cc03f2d457a89deedee0d4
Kenshin <fxblog@gmail.com>
2017-04-13 10:32:43 +0800

    Add remove unread list logic.

--- src/option/unrdist.jsx
+++ src/option/unrdist.jsx
@@ -2,6 +2,8 @@ console.log( "===== simpread option unread list load =====" )
 
 import List from 'list';
 
+import {storage} from 'storage';
+
 const actionItems = [
     {
         id: "pocket",
@@ -21,13 +23,26 @@ const actionItems = [
 
 export default class Unrdist extends React.Component {
 
+    state = {
+        item: this.props.list,
+    }
+
     onAction( event, ...rests ) {
-        console.log( event, rests )
+        const [ id, title, data ] = rests;
+        console.log( id, title, data )
+        if ( id == "remove" ) {
+            storage.UnRead( id, data.idx, success => {
+                success && new Notify().Render( 0, "删除成功" );
+                success && this.setState({
+                    item: storage.unrdist
+                });
+            });
+        }
     }
 
     render() {
         return (
-            <List items={ this.props.list } title={ "未读列表：100 条" } actionItems={ actionItems } onAction={ (e,i,t,d)=>this.onAction(e,i,t,d) } />
+            <List items={ this.state.item } title={ "未读列表：100 条" } actionItems={ actionItems } onAction={ (e,i,t,d)=>this.onAction(e,i,t,d) } />
         )
     }
 }
