875b568f325b767b3be1284d761762e37e181367
Kenshin <fxblog@gmail.com>
2017-04-13 11:47:23 +0800

    Add unread list not found layou and logic.

--- src/option/unrdist.jsx
+++ src/option/unrdist.jsx
@@ -1,6 +1,7 @@
 console.log( "===== simpread option unread list load =====" )
 
-import List from 'list';
+import List     from 'list';
+import Button   from 'button';
 
 import {storage} from 'storage';
 
@@ -19,12 +20,31 @@ const actionItems = [
         disable: false,
         hr: false,
     }
-];
+],
+    style = {
+        root: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        text: {
+            fontSize: '1.6rem',
+            color: '#9b9b9b',
+        },
+        icon : {
+            width: '80px',
+            height: '80px',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundImage: 'url( data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAQAAAD9CzEMAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAHdElNRQfhBA0LLBOn/NkLAAABtklEQVRYw+2YPyxDURTGf5WOGCwiETH7s4kaDJo2FokYMBglZ66lOtGEqWIQm9zdoJouGERIEwNlE4NE4i0iBoO0CxIMvW1f26evvJY86Tfdc3LP99137vvuTa6HqlAwzxI9X064ISrxagweE5mfOXp18CHjoCDJFHbYlBCAOiywGWzLSYmAauUIn6koI+2g/Bzb0gMMyDWoZ9pNuTQByUKLbkSyhD6PyZroYcwiN0xSoQUYJGhZ+FKjwKtlNsggeAEIFJIbnJqm7BKpSeAIgHkdjbKgRwGucgIdOrEuYXOdXKo1Fm3pF+QOQBI6Tqg3wnleb8nUVHmtRNQBQueX5AZbclmRTVFYqBcbSKpS9jtocVLcFGgK/I6AhQ/UEPHCsf09GMyU287KaBc/Xm4vF+YbBv5oDzYd8FXUWrRIQoQa+wV1hfsFmj6whTt9IPvFRv3L39Q51AR7AKzKctMHtnCnD2wE6oAbpgF4bJCA3HKbH7vcaCsSdf9ZVCrQVyfW/uIwtwdZHcXUEzuSccKt2pglpoOsfkpQPs4a0p8ROc+1KI3RAHqDdPExpIv78nPQId7plgf4BCjPbVayklPeAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE3LTA0LTEzVDExOjQ0OjE5KzA4OjAwPYuVdQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxNy0wNC0xM1QxMTo0NDoxOSswODowMEzWLckAAAAASUVORK5CYII= )',
+        }
+};
 
 export default class Unrdist extends React.Component {
 
     state = {
-        item : this.props.list,
+        items: this.props.list,
         title: `未读列表：${ storage.unrdist.length } 条`
     }
 
@@ -34,16 +54,32 @@ export default class Unrdist extends React.Component {
             storage.UnRead( id, data.idx, success => {
                 success && new Notify().Render( 0, "删除成功" );
                 success && this.setState({
-                    item : storage.unrdist,
+                    items: storage.unrdist,
                     title: `未读列表：${ storage.unrdist.length } 条`
                 });
             });
         }
     }
 
+    onClick() {
+        console.log( "Add more storage.unrdist" )
+    }
+
     render() {
+        const content = this.state.items && this.state.items.length > 0 ?
+            <div>
+                <List items={ this.state.items } title={ this.state.title } actionItems={ actionItems } onAction={ (e,i,t,d)=>this.onAction(e,i,t,d) } />
+                <Button type="raised" width="100%" text="加载更多"
+                        color="#fff" backgroundColor="rgb(156, 39, 176)"
+                        waves="sr-button waves-effect waves-button"
+                        onClick={ ()=>this.onClick() } />
+            </div>
+            : <div style={ style.root }>
+                <span style={ style.icon }></span>
+                <span style={ style.text }>没有任何未读列表!</span>
+              </div>;
         return (
-            <List items={ this.state.item } title={ this.state.title } actionItems={ actionItems } onAction={ (e,i,t,d)=>this.onAction(e,i,t,d) } />
+            <div>{ content }</div>
         )
     }
 }
