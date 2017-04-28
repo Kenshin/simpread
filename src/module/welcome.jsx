ca303787127ad5012e84e498c7089c96a8dcf62b
Kenshin <fxblog@gmail.com>
2017-04-28 14:58:23 +0800

    Add welcome copywrite.

--- src/module/welcome.jsx
+++ src/module/welcome.jsx
@@ -66,20 +66,49 @@ class Welcome extends React.Component {
             <welcome>
                 <div className="carousel carousel-slider" data-indicators="true">
                     <div className="carousel-item">
-                        <h2>First Panel</h2>
-                        <div className="desc">This is your first panel</div>
+                        <section>
+                            <img src="chrome-extension://ljmlbfffbjjoeknbipaiilcijbbdchne/assets/images/welcome-0.png"/>
+                            <h2>欢迎使用 简悦</h2>
+                            <div className="desc">
+                                去掉干扰元素，提升阅读体验，<strong>简</strong>单阅读，<strong>愉</strong>悦心情。<br/>
+                                为了达到 <strong>完美</strong> 的阅读模式，简悦适配了 <strong>120+</strong> 个网站。
+                            </div>
+                        </section>
                     </div>
                     <div className="carousel-item">
-                        <h2>Second Panel</h2>
-                        <div className="desc">This is your second panel</div>
+                        <section>
+                            <img src="chrome-extension://ljmlbfffbjjoeknbipaiilcijbbdchne/assets/images/welcome-0.png"/>
+                            <h2>阅读模式</h2>
+                            <div className="desc">
+                                自动提取适配页面的标题、描述、正文、媒体 （ 图片/视频 ） 等资源。<br/>
+                                定制化生成更适合中文阅读的页面。
+                            </div>
+                        </section>
                     </div>
                     <div className="carousel-item">
-                        <h2>Third Panel</h2>
-                        <div className="desc">This is your third panel</div>
+                        <section>
+                            <img src="chrome-extension://ljmlbfffbjjoeknbipaiilcijbbdchne/assets/images/welcome-0.png"/>
+                            <h2>聚焦模式</h2>
+                            <div className="desc">
+                                自动高亮鼠标所在的文章段落，不改变当前页面的结构，适合未适配的网站。
+                            </div>
+                        </section>
                     </div>
                     <div className="carousel-item">
-                        <h2>Fourth Panel</h2>
-                        <div className="desc">This is your fourth panel</div>
+                        <section>
+                            <img src="chrome-extension://ljmlbfffbjjoeknbipaiilcijbbdchne/assets/images/welcome-0.png"/>
+                            <h2>高度定制化</h2>
+                            <div className="desc">
+                                字体样式/大小、版本设计、主题均可设定。<br/>
+                                页面上任意元素（ 不想显示的文字 / 图片 ）均可隐藏。
+                            </div>
+                        </section>
+                    </div>
+                    <div className="carousel-item">
+                        <section>
+                            <img src="chrome-extension://ljmlbfffbjjoeknbipaiilcijbbdchne/assets/images/welcome-0.png"/>
+                            <h2>更多功能等你发现！</h2>
+                        </section>
                     </div>
                 </div>
                 <footer>
