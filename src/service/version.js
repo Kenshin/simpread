4a7f5cd27378e46f2081c392e06c5b1210f52bf0
Kenshin <fxblog@gmail.com>
2017-05-03 15:18:36 +0800

    Add version.Verify() logic.

--- src/service/version.js
+++ src/service/version.js
@@ -2,8 +2,36 @@ console.log( "=== simpread version load ===" )
 
 import {browser} from 'browser';
 
+/**
+ * Manifest.json version
+ */
 const version = browser.runtime.getManifest().version;
 
+/**
+ * Verify version
+ * 
+ * @param {string} local version
+ * @param {object} simpread data structure
+ */
+function Verify( curver, data ) {
+
+    /*
+    if ( curver == "1.0.0" ) {
+        data.option.pocket = { "consumer": "", "access": "" };
+        curver = "1.0.1";
+    }
+
+    if ( curver == "1.0.1" ) {
+        data.read.custom = "";
+        curver = "2.0.0";
+    }
+    */
+
+    data.version = version;
+    return data;
+}
+
 export {
-    version
+    version,
+    Verify
 }
