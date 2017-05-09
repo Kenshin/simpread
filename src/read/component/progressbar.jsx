0124bf96888bc76755968d6ee4e2f59d2b0d1e8e
Kenshin <fxblog@gmail.com>
2017-05-09 16:42:10 +0800

    Add progress component to progressbar.

--- src/read/component/progressbar.jsx
+++ src/read/component/progressbar.jsx
@@ -1,49 +1,56 @@
 console.log( "==== simpread read component: ProcessBar ====" )
 
-import ProgressBar from 'progressbar';
+import Progress from 'progress';
 
-let bar;
+const options = {
+    strokeWidth: 4,
+    easing     : "easeInOut",
+    duration   : 1000,
+    trailColor : "#fff",
+    trailWidth : 0,
+    svgStyle   : {
+        width  : "100%",
+        height : "100%",
+        display: "block",
+        top    : "0",
+    },
+    from       : { color: "#64B5F6" },
+    to         : { color: "#304FFE" },
+    step       : ( state, bar ) => {
+        bar.path.setAttribute( "stroke", state.color );
+    },
+};
 
 export default class ProcessBar extends React.Component {
 
+    static defaultProps = {
+        offset : document.body.scrollTop / ( document.body.scrollHeight - document.documentElement.clientHeight )
+    }
+
+    state = {
+        progress: this.props.offset
+    }
+
     componentDidMount() {
-        $(document).on( "scroll", scrollEventHandle );
-        bar = new ProgressBar.Line( "read-process", {
-            strokeWidth: 4,
-            easing     : "easeInOut",
-            duration   : 1000,
-            trailColor : "#fff",
-            trailWidth : 0,
-            svgStyle   : {
-                width  : "100%",
-                height : "100%",
-                display: "block",
-                top    : "0",
-            },
-            from       : { color: "#64B5F6" },
-            to         : { color: "#304FFE" },
-            step: (state, bar) => {
-                bar.path.setAttribute( "stroke", state.color );
-            },
-        });
-        bar.animate( 0 );
+        $( document ).on( "scroll", ()=>this.scrollEventHandle() );
     }
 
     componentWillUnmount() {
-        $(document).off( "scroll", scrollEventHandle );
+        $( document ).off( "scroll", ()=>this.scrollEventHandle() );
+    }
+
+    scrollEventHandle() {
+        const offset  = document.body.scrollTop / ( document.body.scrollHeight - document.body.clientHeight );
+        this.setState({ progress: offset });
     }
 
     render() {
         return (
-            <read-process></read-process>
+            <Progress type="line" progress={ this.state.progress } options={ options }>
+                <read-process></read-process>
+            </Progress>
         )
+
     }
 
-}
-/**
- * Scroll event handle
- */
-function scrollEventHandle() {
-    const offset = document.body.scrollTop / ( document.body.scrollHeight - document.documentElement.clientHeight );
-    offset && bar.animate( offset );
 }
