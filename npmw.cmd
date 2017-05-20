4d27a3cb9208c10cd771a859ea9155d16f73df30
Kenshin <fxblog@gmail.com>
2017-05-20 14:27:55 +0800

    Modify npm run develop and npm run publish.

--- npmw.cmd
+++ npmw.cmd
@@ -2,7 +2,7 @@
 :: SimpRead : SimpRead development/publish/server environment
 :: HOST     : https://github.com/kenshin/simpread
 :: Author   : Kenshin<kenshin@ksria.com>
-:: Version  : 0.0.2
+:: Version  : 0.0.1
 ::===========================================================
 
 @echo off
@@ -15,8 +15,6 @@ if "%1" == "run" (
          goto develop
     ) else if "%2" == "publish"  (
         goto publish
-    ) else if "%2" == "server"  (
-        goto server
     ) else (
         @echo npmw noly support command: `npmw run develop` and `npmw run publish`.
         goto quit
@@ -30,9 +28,9 @@ if "%1" == "run" (
 :: develop : Development environment
 ::===========================================================
 :develop
-@echo webpack --devtool=source-map --progress --colors --watch
+@echo webpack-dev-server --content-base src --hot --inline --progress --colors --devtool=source-map
 set NODE_ENV=development
-webpack --devtool=source-map --progress --colors --watch
+webpack-dev-server --content-base src --progress --colors --devtool=source-map
 goto quit
 
 ::===========================================================
@@ -42,15 +40,6 @@ goto quit
 @echo webpack -p --progress --colors
 set NODE_ENV=production
 webpack -p --progress --colors
-goto quit
-
-::===========================================================
-:: server : Website
-::===========================================================
-:server
-@echo webpack-dev-server --content-base src --hot --inline --progress --colors --devtool=source-map
-set NODE_ENV=development
-webpack-dev-server --content-base src --progress --colors --devtool=source-map
 goto quit
 
 ::===========================================================
