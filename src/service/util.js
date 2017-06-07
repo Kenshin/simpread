63c03cf256c4e4649ee2f3e17960554881d8006c
Kenshin <fxblog@gmail.com>
2017-06-07 14:14:54 +0800

    Fix verify html regexp bug.

--- src/service/util.js
+++ src/service/util.js
@@ -12,7 +12,7 @@ console.log( "=== simpread util load ===" )
 function verifyHtml( html ) {
     if ( html == "" ) return [ 0, html ];
     else if ( specTest( html )) return [ 2, html ];
-    const item = html.match( /<\S+ (class|id)=("|')[\w-_]+|<[^/]\S+>/ig );
+    const item = html.match( /<\S+ (class|id)=("|')[\w-_]+("|')>$|<[^/]\S+>$/ig );
     if ( item && item.length > 0 ) {
         return [ 1, item ];
     } else {
