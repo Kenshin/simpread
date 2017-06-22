4f0400047505491debf1fa8089c6bc49c037f6da
Kenshin <fxblog@gmail.com>
2017-06-22 10:35:30 +0800

    Modify storage.GetNewsites() remote logic.

--- src/module/common.jsx
+++ src/module/common.jsx
@@ -75,11 +75,9 @@ export default class CommonOpt extends React.Component {
     }
 
     newsites() {
-        storage.GetNewsites( "remote", ( { count, forced }, error ) => {
+        storage.GetNewsites( "remote", ( { count }, error ) => {
             if ( !error ) {
-                count  > 0 && new Notify().Render( 0, `同步更新成功，新更新 ${ count } 个站点。` );
-                forced > 0 && new Notify().Render( 0, `同步更新成功，强制更新 ${forced } 个站点。` );
-                count == 0 && forced == 0 && new Notify().Render( 0, "暂无更新。" );
+                count == 0 ? new Notify().Render( 0, "本地已更新到最新列表，无需更新。" ) : new Notify().Render( 0, `同步更新成功，新更新 ${ count } 个站点。` );
             } else {
                 new Notify().Render( 3, `同步时发生了一些问题，并不会影响本地配置文件，请稍后再试！` );
             }
