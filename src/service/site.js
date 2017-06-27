ed5ac7ad9e88f188ea97a1e4e5fc16b90e974e09
Kenshin <fxblog@gmail.com>
2017-06-27 11:44:35 +0800

    Optimize minimatch logic.

--- src/service/site.js
+++ src/service/site.js
@@ -46,10 +46,10 @@ function findSitebyURL( sites, url ) {
         if ( name == "baijia.baidu.com" && url.includes(name) ) {
             found = cur;
             break;
-        } else if ( !isroot() && !cur.endsWith( "*" ) && cur.replace( /^http[s]?:/, "" ) == url.replace( /^http[s]?:/, "" ) ) {
+        } else if ( cur.match( /\*/g ) && cur.match( /\*/g ).length == 1 && !isroot() && !cur.endsWith( "*" ) && cur.replace( /^http[s]?:/, "" ) == url.replace( /^http[s]?:/, "" ) ) {
             found = cur;
             break;
-        } else if ( !isroot() && cur.endsWith( "*" ) && uri.includes( sufname ) && hostname == sufname && url.includes( name ) ) {
+        } else if ( cur.match( /\*/g ) && cur.match( /\*/g ).length == 1 && !isroot() && cur.endsWith( "*" ) && uri.includes( sufname ) && hostname == sufname && url.includes( name ) ) {
             //if ( /\/[a-zA-Z0-9]+\/\*/g.test( cur )) {
             //    if    ( suffix != url ) return undefined;
             //} else if ( suffix == url ) return undefined;
