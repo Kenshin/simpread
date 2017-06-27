e23dfa91be7cfa2a8d4bd7e9db579b2b399637d6
Kenshin <fxblog@gmail.com>
2017-06-27 13:43:24 +0800

    Add 1.0.0 → 1.0.1 and import 1.0.0 fixed simpread.read.sites url.

--- src/background.js
+++ src/background.js
@@ -19,6 +19,8 @@ storage.Read( () => {
     else {
         !local.Count() && storage.GetNewsites( "remote", getNewsitesHandler );
         ver.version != storage.version && storage.GetNewsites( "local", getNewsitesHandler );
+        ver.version != storage.version && ver.version == "1.0.1" &&
+            ( storage.read.sites = storage.Fix( storage.read.sites, storage.version ));
         ver.version != storage.version && storage.Write( () => {
                 local.Version( ver.version );
                 browser.tabs.create({ url: browser.extension.getURL( "options/options.html#update?ver=" + ver.version ) });
