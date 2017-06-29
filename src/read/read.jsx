6cc4ef9bcd81f9503e9a4435dfd5bb518f826562
Kenshin <fxblog@gmail.com>
2017-06-29 18:42:13 +0800

    Fix initialize use esc shortcuts html style not exist bug.

--- src/read/read.jsx
+++ src/read/read.jsx
@@ -71,8 +71,8 @@ class Read extends React.Component {
 
     componentWillUnmount() {
         $root.removeClass( theme )
-             .removeClass( "simpread-font" )
-             .attr( "style", $root.attr("style").replace( "font-size: 62.5%!important", "" ));
+             .removeClass( "simpread-font" );
+        $root.attr("style") && $root.attr( "style", $root.attr("style").replace( "font-size: 62.5%!important", "" ));
         $( "body" ).removeClass( "simpread-hidden" );
         $( rdclsjq ).remove();
         tooltip.Exit( rdclsjq );
