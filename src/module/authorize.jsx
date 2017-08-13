c318f97bcb7ec62adfa598148019a299be62ce69
Kenshin <fxblog@gmail.com>
2017-08-13 15:09:23 +0800

    Add auth/unauth pocket UI and logic.

--- src/module/authorize.jsx
+++ src/module/authorize.jsx
@@ -35,6 +35,33 @@ export default class Auth extends React.Component {
                     }, storage.secret );
                 }
                 break;
+            case "pocket":
+                if ( value ) {
+                    exp.pocket.Request( ( result, error ) => {
+                        exp.pocket.Redirect( result.code ).done( () => {
+                            exp.pocket.Auth( ( result, error ) => {
+                                storage.secret.pocket.access_token = result.access_token;
+                                storage.secret.pocket.username     = result.username;
+                                storage.Safe( ()=> {
+                                    new Notify().Render( "已成功授权 Pocket 。" );
+                                    this.setState({ secret: storage.secret });
+                                }, storage.secret );
+                                    });
+                        }).fail( error => {
+                            console.error( error )
+                            new Notify().Render( 2, "获取 Pocket 授权失败，请重新获取。" );
+                        });
+                    });
+                        
+                }
+                else {
+                    storage.secret.pocket.access_token = "";
+                    storage.Safe( ()=> {
+                        new Notify().Render( "已取消对 Dropbox 的授权。" );
+                        this.setState({ secret: storage.secret });
+                    }, storage.secret );
+                }
+                break;
         }
     }
 
@@ -52,7 +79,14 @@ export default class Auth extends React.Component {
                             thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
                             label={ this.state.secret.dropbox.access_token ? "已授权 Dropbox，是否取消授权？" : "是否连接并授权 Dropbox ？" }
                             onChange={ (s)=>this.onChange( "dropbox", s ) } />
-                  </div>;
+
+
+                        <Switch width="100%" checked={ this.state.secret.pocket.access_token != "" ? true : false }
+                            thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
+                            label={ this.state.secret.pocket.access_token ? "已授权 Pocket，是否取消授权？" : "是否连接并授权 Pocket ？" }
+                            onChange={ (s)=>this.onChange( "pocket", s ) } />
+
+                    </div>;
         } 
 
         return (
