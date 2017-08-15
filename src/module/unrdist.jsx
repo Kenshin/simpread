8f6ea8cba7a71ac2a380669b16a03b2ec4ed2998
Kenshin <fxblog@gmail.com>
2017-08-15 11:42:34 +0800

    Optimize export.linnk.Add() workflow.

--- src/module/unrdist.jsx
+++ src/module/unrdist.jsx
@@ -79,11 +79,17 @@ export default class Unrdist extends React.Component {
             storage.Safe( ()=> {
                 if ( storage.secret.linnk.access_token ) {
                     exp.linnk.access_token = storage.secret.linnk.access_token;
-                    exp.linnk.group_id     = storage.secret.linnk.group_id;
-                    exp.linnk.Add( data.url, data.title.trim(), ( result, error ) => {
-                        !error && result.code == 200 && new Notify().Render( "已成功保存到 Linnk！" );
-                        error  && new Notify().Render( 2, "保存失败，请稍后重新再试。" );
-                    });
+                    exp.linnk.GetSafeGroup( storage.secret.linnk.group_name, ( result, error ) => {
+                        if ( !error ) {
+                            exp.linnk.group_id = result.data.groupId;
+                            exp.linnk.Add( data.url, data.title.trim(), ( result, error ) => {
+                                !error && result.code == 200 && new Notify().Render( "已成功保存到 Linnk！" );
+                                error  && new Notify().Render( 2, "保存失败，请稍后重新再试。" );
+                            });
+                        } else {
+                            new Notify().Render( 2, "保存失败，请稍后重新再试。" );
+                        }
+                    })
                 } else {
                     new Notify().Render( "请先获取 Linnk 的授权，才能使用此功能！", "授权", ()=>{
                         browser.runtime.sendMessage( msg.Add( msg.MESSAGE_ACTION.new_tab, { url: browser.extension.getURL( "options/options.html#labs" ) } ));
