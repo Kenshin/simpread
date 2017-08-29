e316dcd4d42359207afd65c951c7990f2bc40b25
Kenshin <fxblog@gmail.com>
2017-08-29 09:22:19 +0800

    Remove old hashnotify: auth.

--- src/options/options.js
+++ src/options/options.js
@@ -102,9 +102,6 @@ function hashnotify() {
             case "sync":
                 new Notify().Render( 0, "数据同步成功！" );
                 break;
-            case "auth":
-                new Notify().Render( 0, "授权成功！" );
-                break;
             default:
                 // TO-DO
         }
