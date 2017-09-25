724c6570913a218dc5b33d0f08088cb10c70a053
Kenshin <fxblog@gmail.com>
2017-09-25 16:29:56 +0800

    Add pdf action.

--- src/focus/controlbar.jsx
+++ src/focus/controlbar.jsx
@@ -31,7 +31,7 @@ class FControl extends React.Component {
                 break;
             default:
                 if ( type.indexOf( "_" ) > 0 && type.startsWith( "share" ) || 
-                     [ "save", "markdown", "png", "kindle", "dropbox", "pocket", "linnk", "yinxiang","evernote", "onenote", "gdrive" ].includes( type )) {
+                     [ "save", "markdown", "png", "pdf", "kindle", "dropbox", "pocket", "linnk", "yinxiang","evernote", "onenote", "gdrive" ].includes( type )) {
                     const [ title, desc, content ] = [ $( "head title" ).text().trim(), "", $( ".simpread-focus-highlight" ).html().trim() ];
                     output.Action( type, title, desc, content );
                 }
