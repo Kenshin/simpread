46b1491e2960d9808d475a81ed1f9c124560eb9a
Kenshin <fxblog@gmail.com>
2017-09-25 11:52:00 +0800

    Add highlight notif message.

--- src/focus/focus.js
+++ src/focus/focus.js
@@ -118,7 +118,7 @@ var storage  = require( "storage" ).storage,
             console.error( "Get $focus failed", error )
         }
         if ( true ) {
-            new Notify().Render( "已启动手动聚焦模式，请移动鼠标进行选择。" );
+            new Notify().Render( "已启动手动聚焦模式，请移动鼠标进行选择, ESC 退出。" );
             highlight.Start().done( result => {
                 $focus = $( result );
                 dtd.resolve( $focus );
