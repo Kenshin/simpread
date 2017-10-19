4ffed959896d75cf2fd0e6edfd4a60272f00747b
Kenshin <fxblog@gmail.com>
2017-10-19 15:10:04 +0800

    Add option.secret to labs.

--- src/module/labs.jsx
+++ src/module/labs.jsx
@@ -63,6 +63,11 @@ export default class LabsOpt extends React.Component {
                             tooltip={{ text: "默认点击后弹出设定对话框" }}
                             label="动作栏图标是否改为 「进入/退出阅读模式 」？"
                             onChange={ (s)=>this.onChange(s, "option", "br_exit") } />
+                    <Switch width="100%" checked={ this.props.option.secret }
+                            thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
+                            tooltip={{ text: "默认不同步，选中同步后，请妥善保管你的授权码。" }}
+                            label="同步时是否包含授权服务中的授权码？"
+                            onChange={ (s)=>this.onChange(s, "option", "secret") } />
                 </div>
 
                 <div className="label">右键菜单</div>
