abf07d086f0a8fffbb6b65928688d903e5f2cb39
Kenshin <fxblog@gmail.com>
2017-10-19 15:25:37 +0800

    Fix focus mode and read mode controlbar not watch bug.

--- src/read/controlbar.jsx
+++ src/read/controlbar.jsx
@@ -6,6 +6,7 @@ import * as msg    from 'message';
 import th          from 'theme';
 import * as config from 'config';
 import * as output from 'output';
+import * as watch  from 'watch';
 
 import Fab         from 'fab';
 
@@ -29,47 +30,57 @@ export default class ReadCtlbar extends React.Component {
     onAction( event, type ) {
         console.log( "fab type is =", type )
 
-        this.props.multi && 
+        const action = ( event, type ) => {
+            this.props.multi && 
             [ "markdown", "dropbox", "yinxiang","evernote", "onenote", "gdrive" ].includes( type ) &&
             new Notify().Render( "当前为论坛类页面，不建议使用导出服务，有可能出现未知错误。" );
 
-        switch ( true ) {
-            case [ "exit", "setting" ].includes( type ):
-                this.props.onAction( type );
-                break;
-            /*
-            case [ "up", "down" ].includes( type ):
-                this.props.onAction && this.props.onAction( "scroll", type == "up" ? -250 : 250 );
-                break;
-            */
-            case type.indexOf( "_" ) > 0 && ( type.startsWith( "fontfamily" ) || type.startsWith( "fontsize" ) || type.startsWith( "layout" )):
-                if ( !ss.VerifyCustom( type.split( "_" )[0], this.props.custom ) ) {
-                    const [ key, value ] = [ type.split( "_" )[0], type.split( "_" )[1] ];
-                    Object.keys( ss ).forEach( (name)=>name.toLowerCase() == key && ss[name]( value ));
-                    this.props.onAction && this.props.onAction( key, value );    
-                } else {
-                    new Notify().Render( '由于已使用 自定义样式，因此当前操作无效，详细说明 <a href="https://github.com/Kenshin/simpread/wiki/自定义样式" target="_blank">请看这里</a>' );
-                }
-                break;
-            case type.indexOf( "_" ) > 0 && type.startsWith( "theme" ):
-                if ( !ss.VerifyCustom( type.split( "_" )[0], this.props.custom ) ) {
-                    let i = th.names.indexOf( th.theme );
-                    i = type.endsWith( "prev" ) ? --i : ++i;
-                    i >= th.names.length && ( i = 0 );
-                    i < 0 && ( i = th.names.length - 1 );
-                    th.Change( th.names[i] );
-                    this.props.onAction && this.props.onAction( type.split( "_" )[0], th.theme );
-                } else {
-                    new Notify().Render( '由于已使用 自定义样式，因此当前操作无效，详细说明 <a href="https://github.com/Kenshin/simpread/wiki/自定义样式" target="_blank">请看这里</a>' );
-                }
-                break;
-            default:
-                if ( type.indexOf( "_" ) > 0 && type.startsWith( "share" ) || 
-                    [ "save", "markdown", "png", "pdf", "kindle", "dropbox", "pocket", "linnk", "yinxiang","evernote", "onenote", "gdrive" ].includes( type )) {
-                    const [ title, desc, content ] = [ $( "sr-rd-title" ).text().trim(), $( "sr-rd-desc" ).text().trim(), $( "sr-rd-content" ).html().trim() ];
-                    output.Action( type, title, desc, content );
-                }
-        }
+            switch ( true ) {
+                case [ "exit", "setting" ].includes( type ):
+                    this.props.onAction( type );
+                    break;
+                /*
+                case [ "up", "down" ].includes( type ):
+                    this.props.onAction && this.props.onAction( "scroll", type == "up" ? -250 : 250 );
+                    break;
+                */
+                case type.indexOf( "_" ) > 0 && ( type.startsWith( "fontfamily" ) || type.startsWith( "fontsize" ) || type.startsWith( "layout" )):
+                    if ( !ss.VerifyCustom( type.split( "_" )[0], this.props.custom ) ) {
+                        const [ key, value ] = [ type.split( "_" )[0], type.split( "_" )[1] ];
+                        Object.keys( ss ).forEach( (name)=>name.toLowerCase() == key && ss[name]( value ));
+                        this.props.onAction && this.props.onAction( key, value );    
+                    } else {
+                        new Notify().Render( '由于已使用 自定义样式，因此当前操作无效，详细说明 <a href="https://github.com/Kenshin/simpread/wiki/自定义样式" target="_blank">请看这里</a>' );
+                    }
+                    break;
+                case type.indexOf( "_" ) > 0 && type.startsWith( "theme" ):
+                    if ( !ss.VerifyCustom( type.split( "_" )[0], this.props.custom ) ) {
+                        let i = th.names.indexOf( th.theme );
+                        i = type.endsWith( "prev" ) ? --i : ++i;
+                        i >= th.names.length && ( i = 0 );
+                        i < 0 && ( i = th.names.length - 1 );
+                        th.Change( th.names[i] );
+                        this.props.onAction && this.props.onAction( type.split( "_" )[0], th.theme );
+                    } else {
+                        new Notify().Render( '由于已使用 自定义样式，因此当前操作无效，详细说明 <a href="https://github.com/Kenshin/simpread/wiki/自定义样式" target="_blank">请看这里</a>' );
+                    }
+                    break;
+                default:
+                    if ( type.indexOf( "_" ) > 0 && type.startsWith( "share" ) || 
+                        [ "save", "markdown", "png", "pdf", "kindle", "dropbox", "pocket", "linnk", "yinxiang","evernote", "onenote", "gdrive" ].includes( type )) {
+                        const [ title, desc, content ] = [ $( "sr-rd-title" ).text().trim(), $( "sr-rd-desc" ).text().trim(), $( "sr-rd-content" ).html().trim() ];
+                        output.Action( type, title, desc, content );
+                    }
+            }
+        };
+
+        type != "exit" ? watch.Verify( ( state, result ) => {
+            if ( state ) {
+                console.log( "watch.Lock()", result );
+                new Notify().Render( "配置文件已更新，刷新当前页面后才能生效。", "刷新", ()=>location.href = location.href );
+            } else action( event, type );
+        }) : action( event, type );
+
     }
 
     bindShortcuts() {
