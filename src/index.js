93bc62cf83d4b2075733638cf12fe24e63c0cc4d
Kenshin <fxblog@gmail.com>
2017-10-21 15:18:02 +0800

    Add Google analytics.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// Import css
 import 'main';
+
+// Google analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-405976-9');
