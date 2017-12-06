001477e952d33f1fb8ed387663a933ca0062cff5
Kenshin <fxblog@gmail.com>
2017-12-06 15:05:50 +0800

    Format source.

--- src/module/siteeditor.jsx
+++ src/module/siteeditor.jsx
@@ -26,7 +26,7 @@ class SiteEditor extends React.Component {
 
     delete() {
         console.log( "siteeditor click delete button.", storage.current.site )
-        storage.Deletesite( storage.current.mode, site, result => {
+        storage.Deletesite( "read", site, result => {
             if ( result == -1 ) new Notify().Render( 2, "删除失败，请刷新本页再试试。" );
             else {
                 new Notify().Render( "删除成功，如需生效，请刷新本页。" );
