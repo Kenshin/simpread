b954aea6245b0a710439f326ab3bc80106fe871d
Kenshin <fxblog@gmail.com>
2017-12-11 10:52:13 +0800

    Format source.

--- src/module/modals.jsx
+++ src/module/modals.jsx
@@ -34,7 +34,7 @@ class Modals extends React.Component {
     save() {
         console.log( "modals click submit button.", storage.current, flag )
         if ( storage.current.mode == "focus" ) {
-            new Notify().Render( 3, "测试版 1.0.6 所以暂时不支持聚焦模式下的保存功能，正式版时将会恢复。" );
+            new Notify().Render( 3, "测试版 1.0.6 暂时不支持聚焦模式下的保存功能，正式版时将会恢复。" );
             return;
         }
         if ( Object.values( flag ).findIndex( key => key != 0 ) != -1 ) {
