2a7e8201809533d326dbdddbeecafbca3409d722
Kenshin <fxblog@gmail.com>
2017-12-22 12:07:21 +0800

    Remove progressbar.min.js from webpack.config.

--- src/read/progressbar.jsx
+++ src/read/progressbar.jsx
@@ -1,28 +1,5 @@
 console.log( "==== simpread read component: ProcessBar ====" )
 
-import Progress from 'progress';
-
-/*
-const options = {
-    strokeWidth: 4,
-    easing     : "easeInOut",
-    duration   : 1000,
-    trailColor : "#fff",
-    trailWidth : 0,
-    svgStyle   : {
-        width  : "100%",
-        height : "100%",
-        display: "block",
-        top    : "0",
-    },
-    from       : { color: "#64B5F6" },
-    to         : { color: "#304FFE" },
-    step       : ( state, bar ) => {
-        bar.path.setAttribute( "stroke", state.color );
-    },
-};
-*/
-
 export default class ProcessBar extends React.Component {
 
     static defaultProps = {
@@ -54,15 +31,6 @@ export default class ProcessBar extends React.Component {
         return (
             this.props.show && <read-process style={{ "width": `${progress}%` }}></read-process>
         )
-        /*
-        const progress = !Number.isFinite( this.state.progress ) ? 0 : this.state.progress;
-        return (
-            this.props.show && <Progress type="line" progress={ progress } options={ options }>
-                <read-process></read-process>
-            </Progress>
-        )
-        */
-
     }
 
 }
