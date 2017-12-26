74476791845e7ab17b3593803534cdfc9f32ebe7
Kenshin <fxblog@gmail.com>
2017-12-26 15:25:09 +0800

    Format source.

--- src/module/modals.jsx
+++ src/module/modals.jsx
@@ -53,7 +53,7 @@ class Modals extends React.Component {
                         storage.Cleansite( storage.current.site );
                         storage.Setcur( storage.current.mode, changed.st.length > 0 ? true : false );
                         browser.runtime.sendMessage( msg.Add( msg.MESSAGE_ACTION.shortcuts, { url: window.location.href } ));
-                        new Notify().Render( 0, "更新成功！" )
+                        new Notify().Render( 0, "更新成功，刷新当前页面后才能生效！" )
                         this.close( false );
                     }
                 }
