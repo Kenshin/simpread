b99f1629e40086dda0c1704ab9bd0d11f36daaf8
Kenshin <fxblog@gmail.com>
2018-04-26 12:13:10 +0800

    Optimize style set and get logic.

--- src/vender/mduikit/switch.jsx
+++ src/vender/mduikit/switch.jsx
@@ -12,8 +12,6 @@
 
 console.log( "==== simpread component: Switch ====" )
 
-let styles = new Map();
-
 const color           = "rgba(51, 51, 51, .87)",
       secondary_color = "rgba(204, 204, 204, 1)",
 
@@ -207,9 +205,10 @@ export default class Switch extends React.Component {
     };
 
     state = {
-        id      : Math.round(+new Date()),
         checked : this.props.checked,
     };
+
+    style = cssinjs();
 
     onClick() {
         !this.props.disable && this.setState({
@@ -223,8 +222,7 @@ export default class Switch extends React.Component {
     }
 
     render() {
-        const style = { ...cssinjs() };
-        styles.set( this.state.id, style );
+        const style = { ...this.style };
 
         this.props.thumbColor   && ( style.thumb_normal.backgroundColor = this.props.thumbColor );
         this.props.thumbedColor && ( style.thumbed.backgroundColor      = this.props.thumbedColor );
