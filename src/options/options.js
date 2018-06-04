2c4b103ef1d99c366fade7a9226ac81075789267
Kenshin <fxblog@gmail.com>
2018-06-04 14:20:37 +0800

    Add content_security_policy and google analytics.

--- src/options/options.js
+++ src/options/options.js
@@ -266,3 +266,16 @@ function pRead() {
     storage.pr.origins = storage.option.origins;
     console.log( "current puread object is   ", storage.pr )
 }
+
+/**
+ * Google analytics
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-405976-12']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
