c4c36a96cd3276852cb0b2aeaa1af85ea77fa1bb
Kenshin <fxblog@gmail.com>
2018-06-05 18:20:39 +0800

    Optimize google analytics code.

--- src/options/options.js
+++ src/options/options.js
@@ -270,12 +270,12 @@ function pRead() {
 /**
  * Google analytics
  */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-405976-12']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+analytics();
+function analytics() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-405976-12', 'auto');
+    ga('send', 'pageview');
+}
