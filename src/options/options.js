8498ee772c3a3435b4bc7b60e1044d2ef691558a
Kenshin <fxblog@gmail.com>
2018-06-08 10:16:26 +0800

    Optimize google analytics.

--- src/options/options.js
+++ src/options/options.js
@@ -268,16 +268,3 @@ function pRead() {
     storage.pr.origins = storage.option.origins;
     console.log( "current puread object is   ", storage.pr )
 }
-
-/**
- * Google analytics
- */
-analytics();
-function analytics() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-405976-12', 'auto');
-    ga('send', 'pageview');
-}
