fdd65115a951947a67ede36212b499d707c595dd
Kenshin <fxblog@gmail.com>
2018-06-11 15:29:42 +0800

    Change some copywrite.

--- src/module/welcome.jsx
+++ src/module/welcome.jsx
@@ -181,7 +181,7 @@ class Welcome extends React.Component {
                                 <h2 style={ style.h2 }>阅读模式 与 聚焦模式</h2>
                                 <div style={ style.desc }>
                                     阅读模式： <strong>独有功能</strong>，自动提取适配页面的标题、描述、正文、媒体等资源。<br/>
-                                    支持 <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%B4%E6%97%B6%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">临时阅读</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/TXT-%E9%98%85%E8%AF%BB%E5%99%A8">TXT 阅读器</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%BB%E5%8A%A8%E9%80%82%E9%85%8D%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">主动适配模式</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E6%99%BA%E8%83%BD%E8%AF%86%E5%88%AB%E6%A8%A1%E5%BC%8F">智能识别模式</a>
+                                    支持 <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%B4%E6%97%B6%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">临时阅读</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/TXT-%E9%98%85%E8%AF%BB%E5%99%A8">TXT 阅读器</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%BB%E5%8A%A8%E9%80%82%E9%85%8D%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">主动适配模式</a> · <a target="_blank" href="https://github.com/kenshin/simpread/wiki/智能适配模式">智能适配模式</a>
                                     · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E8%AE%BA%E5%9D%9B%E7%B1%BB%E9%A1%B5%E9%9D%A2%E5%8F%8A%E5%88%86%E9%A1%B5">论坛类页面 / 分页</a>。<br/>
                                     聚焦模式：高亮鼠标所在的文章段落，不改变当前页面的结构，适合未适配的网站。<br/>
                                 </div>
@@ -234,7 +234,7 @@ class Welcome extends React.Component {
                                 <div style={ style.desc }>
                                     从现在开始，简悦可以识别出 Wordpress · Hexo · Ghost · <br/>
                                     Discuz 等博客 / 论坛的页面了！<br/>
-                                    甚至，只要是结构良好的页面，（无需适配）自动生成阅读模式，详细 <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E6%99%BA%E8%83%BD%E8%AF%86%E5%88%AB%E6%A8%A1%E5%BC%8F">请看这里</a> 。
+                                    甚至，只要是结构良好的页面，（无需适配）自动生成阅读模式，详细 <a target="_blank" href="https://github.com/kenshin/simpread/wiki/智能适配模式">请看这里</a> 。
                                 </div>
                             </section>
                         </div> }
