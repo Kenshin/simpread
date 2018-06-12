9f536f0afe96b2bce069fb8fbf942bea148fae45
Kenshin <fxblog@gmail.com>
2018-06-12 14:04:46 +0800

    Modify some copywrite.

--- src/module/welcome.jsx
+++ src/module/welcome.jsx
@@ -181,7 +181,7 @@ class Welcome extends React.Component {
                                 <h2 style={ style.h2 }>阅读模式 与 聚焦模式</h2>
                                 <div style={ style.desc }>
                                     阅读模式： <strong>独有功能</strong>，自动提取适配页面的标题、描述、正文、媒体等资源。<br/>
-                                    支持 <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%B4%E6%97%B6%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">临时阅读</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/TXT-%E9%98%85%E8%AF%BB%E5%99%A8">TXT 阅读器</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%BB%E5%8A%A8%E9%80%82%E9%85%8D%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">主动适配模式</a> · <a target="_blank" href="https://github.com/kenshin/simpread/wiki/智能适配模式">智能适配模式</a>
+                                    支持 <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%B4%E6%97%B6%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">临时阅读模式</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/TXT-%E9%98%85%E8%AF%BB%E5%99%A8">TXT 阅读模式</a> · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E4%B8%BB%E5%8A%A8%E9%80%82%E9%85%8D%E9%98%85%E8%AF%BB%E6%A8%A1%E5%BC%8F">主动适配模式</a> · <a target="_blank" href="https://github.com/kenshin/simpread/wiki/智能适配模式">智能适配模式</a>
                                     · <a target="_blank" href="https://github.com/Kenshin/simpread/wiki/%E8%AE%BA%E5%9D%9B%E7%B1%BB%E9%A1%B5%E9%9D%A2%E5%8F%8A%E5%88%86%E9%A1%B5">论坛类页面 / 分页</a>。<br/>
                                     聚焦模式：高亮鼠标所在的文章段落，不改变当前页面的结构，适合未适配的网站。<br/>
                                 </div>
