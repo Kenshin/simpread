636aea53fc1ee3e927bed6be856cc2f3a5459772
Kenshin <fxblog@gmail.com>
2018-07-25 10:46:16 +0800

    Format source.

--- src/options/sitemgr.js
+++ src/options/sitemgr.js
@@ -109,12 +109,9 @@ function controlbarRender() {
                   site= pr.Cleansite({ ...cur_site });
             let flag  = -1;
 
-            if ( site.info && state != "remote" && ( type == "update" || type == "delete" )) {
+            if ( site.info && state != "remote" && site.target == "custom" &&( type == "update" || type == "delete" )) {
                 if ( site.info.id.substr(0,8) == storage.user.uid.substr(0,8) ) {
                     setTimeout( ()=>new Notify().Render( 2, "当前站有远程数据，请保持同步更新。" ), 500 );
-                } else {
-                    // other user site
-                    // TO-DO
                 }
             }
 
