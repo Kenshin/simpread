403f7f02db89d1d47a4bde35fbe5e57793a567b0
Kenshin <fxblog@gmail.com>
2018-08-06 16:51:35 +0800

    Format source.

--- src/module/enhancesite.jsx
+++ src/module/enhancesite.jsx
@@ -46,7 +46,7 @@ window.addEventListener( "siteinfochanged", event => {
  */
 function getService( method ) {
     //return ( location.hostname != "simpread.ksria.cn" ? "http://localhost:3000" : "http://simpread.ksria.cn" ) + method;
-    return  "http://simpread.ksria.cn" + method;
+    return "http://simpread.ksria.cn" + method;
 }
 
 /**
