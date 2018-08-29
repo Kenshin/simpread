5ea77a5ff9ddc37b4dcf9db355a8e88e4a086a67
Kenshin <fxblog@gmail.com>
2018-08-29 11:18:49 +0800

    Format source.

--- src/options/options.js
+++ src/options/options.js
@@ -91,7 +91,6 @@ storage.Read( first => {
     mainRender( tabsItemID );
     tt.Render( "body" );
     waves.Render({ root: "body" });
-    welcomeRender( true, "all" )
     // only firefox and only usage 1.1.0.3024
     //if ( br.isFirefox() && ver.sub_ver == "3024" && !localStorage["opt-3024"] ) {
     //    welcomeRender( true );
