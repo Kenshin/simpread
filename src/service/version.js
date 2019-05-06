4125448537caf6130438585a7c4ca67744aaacbc
Kenshin <fxblog@gmail.com>
2019-05-06 10:27:24 +0800

    Format source.

--- src/service/version.js
+++ src/service/version.js
@@ -208,11 +208,11 @@ function Compare( target ) {
  */
 function FixSubver( patch, target ) {
     if ( patch == "5005" ) {
-        target.read.cleanup = true;
-        target.read.pure    = true;
-        target.option.menu.whitelist = false;
-        target.option.menu.exclusion = false;
-        target.option.menu.blacklist = false;
+        target.read.cleanup == undefined && ( target.read.cleanup = true );
+        target.read.pure    == undefined && ( target.read.pure    = true );
+        target.option.menu.whitelist == undefined && ( target.option.menu.whitelist = false );
+        target.option.menu.exclusion == undefined && ( target.option.menu.exclusion = false );
+        target.option.menu.blacklist == undefined && ( target.option.menu.blacklist = false );
     }
     target.patch = patch;
     return target;
