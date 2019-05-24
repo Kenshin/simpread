214dc36e34a52a5875dbf799f1f60fc949299d6b
Kenshin <fxblog@gmail.com>
2019-05-24 15:50:06 +0800

    Add jianguo authorization logic.

--- src/module/authorize.jsx
+++ src/module/authorize.jsx
@@ -20,6 +20,10 @@ export default class Auth extends React.Component {
         instapaper: {
             username: "",
             password: "",
+        },
+        jianguo: {
+            username: "",
+            password: "",
         }
     }
 
@@ -27,11 +31,12 @@ export default class Auth extends React.Component {
         secret : undefined,
         linnk  : undefined,
         instapaper : undefined,
+        jianguo: undefined,
     }
 
     onChange( state, value, flag ) {
         let notify;
-        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive } = exp,
+        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive, jianguo } = exp,
             clear = ( id, name ) => {
                 Object.keys( storage.secret[id] ).forEach( item => storage.secret[id][item] = "" );
                 storage.Safe( ()=> {
@@ -42,10 +47,12 @@ export default class Auth extends React.Component {
             success = ( id, name, data ) => {
                 notify && notify.complete();
                 Object.keys( data ).forEach( item => storage.secret[id][item] = data[item] );
+                id == "jianguo" && ( storage.secret[id]["access_token"] = { username: data.username, password: data.password });
                 storage.Safe( () => {
                     new Notify().Render( `已成功授权 ${name} 。` );
                     id == "linnk"      && this.setState({ secret: storage.secret, linnk: false });
                     id == "instapaper" && this.setState({ secret: storage.secret, instapaper: false });
+                    id == "jianguo"    && this.setState({ secret: storage.secret, jianguo: false });
                     if ( location.hash.startsWith( "#labs?auth=" ) ) {
                         new Notify().Render( "3 秒钟将会关闭此页面..." );
                         setTimeout( () => {
@@ -72,6 +79,11 @@ export default class Auth extends React.Component {
             return;
         }
 
+        if ( state == "jianguo" && !flag && !storage.secret.jianguo.username ) {
+            this.setState({ jianguo: !this.state.jianguo });
+            return;
+        }
+
         if ( !value ) {
             state == "pocket" && $( this.refs.pocket_tags ).velocity( value ? "slideDown" : "slideUp" );
             if ( state == "linnk" ) {
@@ -81,6 +93,10 @@ export default class Auth extends React.Component {
             if ( state == "instapaper" ) {
                 this.props.instapaper.username = "";
                 this.props.instapaper.password = "";
+            }
+            if ( state == "jianguo" ) {
+                this.props.jianguo.username = "";
+                this.props.jianguo.password = "";
             }
             clear( state, exp.Name( state ));
             return;
@@ -170,6 +186,13 @@ export default class Auth extends React.Component {
                     });
                 }).fail( error => failed( error, gdrive.id, gdrive.name ));
                 break;
+            case "jianguo":
+                jianguo.Auth( this.props.jianguo.username, this.props.jianguo.password, result => {
+                    if ( result && result.done ) {
+                        success( "jianguo", "坚果云", { username: this.props.jianguo.username, password: this.props.jianguo.password } );
+                    } else failed( result.error, jianguo.id, jianguo.name );
+                });
+                break;
         }
     }
 
@@ -185,6 +208,10 @@ export default class Auth extends React.Component {
 
     instapaperOnChange( state, value ) {
         this.props.instapaper[state] = value;
+    }
+
+    jianguoOnChange( state, value ) {
+        this.props.jianguo[state] = value;
     }
 
     componentWillReceiveProps( nextProps ) {
@@ -304,6 +331,33 @@ export default class Auth extends React.Component {
                             thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
                             label={ this.state.secret.gdrive.access_token ? "已授权 Google 云端硬盘，是否取消授权？" : "是否连接并授权 Google 云端硬盘 ？" }
                             onChange={ (s)=>this.onChange( "gdrive", s ) } />
+
+                        <Switch width="100%" checked={ this.state.secret.jianguo && this.state.secret.jianguo.username != "" && this.state.secret.jianguo.password ? true : false }
+                            thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
+                            label={ this.state.secret.jianguo && this.state.secret.jianguo.username != "" ? "已授权 坚果云，是否取消授权？" : "是否连接并授权 坚果云 ？" }
+                            onChange={ (s)=>this.onChange( "jianguo", s ) } />
+
+                        { this.state.jianguo && 
+                        <div ref="jianguo">
+                            <div style={{ "display": "flex", "flex-direction": "row" }}>
+                                <TextField
+                                    placeholder="请填入 坚果云的 WebDAV 用户名，简悦不会记录你的用户名。" 
+                                    onChange={ (evt)=>this.jianguoOnChange( "username", evt.target.value ) }
+                                />
+                                <TextField
+                                    password={true}
+                                    placeholder="请填入 坚果云的 WebDAV 密码，简悦不会记录你的密码。" 
+                                    onChange={ (evt)=>this.jianguoOnChange( "password", evt.target.value ) }
+                                />
+                            </div>
+
+                            <Button type="raised" width="100%" style={{ "margin": "0" }}
+                                text="绑定 坚果云 的信息"
+                                color="#fff" backgroundColor="#3F51B5"
+                                waves="md-waves-effect md-waves-button"
+                                onClick={ (s)=>this.onChange( "jianguo", s, "login" ) } />
+
+                        </div> }
 
                     </div>;
         }
