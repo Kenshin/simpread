f7958536c074b98da1ca1595bb8931e501fb2fa6
Kenshin <fxblog@gmail.com>
2019-05-28 18:09:06 +0800

    Add paging logic.

--- src/module/notice.jsx
+++ src/module/notice.jsx
@@ -22,10 +22,21 @@ const write = ( callback ) => {
 
 export default class Notice extends React.Component {
 
+    static defaultProps = {
+        step: 20,
+    };
+
+    static propsType = {
+        step: React.PropTypes.number,
+    };
+
     state = {
         items: [],
         detail: "",
         failed: false,
+
+        total: 0,
+        page : 1,
     }
 
     onClick( event, id ) {
@@ -47,10 +58,14 @@ export default class Notice extends React.Component {
         });
     }
 
+    onLoadmoreClick() {
+        this.setState({ page: this.state.page + 1 });
+    }
+
     componentWillMount() {
         $.ajax( "http://localhost:3000/notice" )
             .done( result => {
-                this.setState({ items: result.notice });
+                this.setState({ items: result.notice, total: Math.ceil( result.notice.length / this.props.step ) });
             })
             .fail( ( jqXHR, textStatus, errorThrown ) => {
                 this.setState({ failed: true });
@@ -62,9 +77,10 @@ export default class Notice extends React.Component {
     }
 
     render() {
-        let dom;
+        let   dom;
+        const items = this.state.items.slice( 0, this.state.page * this.props.step );
         if ( this.state.failed ) {
-            dom = 
+            dom =
                 <notice>
                     <div className="failed">
                         <div dangerouslySetInnerHTML={{__html: `<svg viewBox="0 0 1024 1024" version="1.1" p-id="2377" width="120" height="120"><defs><style type="text/css"></style></defs><path d="M512.041444 243.968477c-148.07343 0-268.049942 120.022561-268.049942 268.051989 0 147.988496 119.976512 268.053012 268.049942 268.053012 147.987472 0 268.051989-120.064516 268.051989-268.053012C780.093433 363.991038 660.028916 243.968477 512.041444 243.968477zM556.716946 690.765453 467.367989 690.765453l0-89.351004 89.348957 0L556.716946 690.765453zM556.716946 556.652989 467.367989 556.652989l0-223.37751 89.348957 0L556.716946 556.652989zM780.093433 65.22349 243.991502 65.22349c-98.774631 0-178.700985 80.101339-178.700985 178.744987l0 536.105001c0 98.730629 79.925331 178.700985 178.700985 178.700985l536.102954 0c98.600669 0 178.615027-79.969333 178.615027-178.700985L958.709483 243.968477C958.70846 145.32483 878.695125 65.22349 780.093433 65.22349zM869.44546 735.397976c0 73.994248-60.033281 134.069485-134.027529 134.069485L288.667004 869.467461c-73.994248 0-134.115534-60.075237-134.115534-134.069485L154.55147 288.599977c0-73.994248 60.121286-133.939525 134.115534-133.939525l446.750927 0c73.994248 0 134.027529 59.945277 134.027529 133.939525L869.44546 735.397976z" p-id="2378" fill="#16666f"></path></svg>` }}></div>
@@ -87,7 +103,16 @@ export default class Notice extends React.Component {
                                 waves="md-waves-effect md-waves-button"
                                 onClick={ ()=>this.onReadallClick() } />
                         </div>
-                        <List list={ this.state.items } onClick={ ( e, id )=> this.onClick( e, id ) } />
+                        <List list={ items } onClick={ ( e, id )=> this.onClick( e, id ) } />
+                        { this.state.page < this.state.total &&
+                            <div className="list controlbar">
+                                <Button type="raised" text="加载更多"
+                                    style={{ "margin": "0" }} width="100%"
+                                    color="#fff" backgroundColor="#16666f"
+                                    waves="md-waves-effect md-waves-button"
+                                    onClick={ ()=>this.onLoadmoreClick() } />
+                            </div>
+                        }
                     </div>
                     <div className="detail">
                         <div className="empty">
