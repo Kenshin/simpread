ff308a84c42af1f8b5a9c6f536d5239038056525
Kenshin <fxblog@gmail.com>
2019-05-31 18:26:36 +0800

    Optimize toc logic.

--- src/read/toc.jsx
+++ src/read/toc.jsx
@@ -38,31 +38,34 @@ class TOC extends React.Component {
               scrollItems   = menuItems.map( function() {
                 const item  = $( $(this).attr("href") );
                     if ( item.length ) { return item; }
-              });
+              }),
+              setActive = function() {
+                if ( is_click ) return;
+                const fromTop = $(window).scrollTop() + topMenuHeight;
+                let cur = scrollItems.map( function() {
+                if ($(this).offset().top < fromTop)
+                    return this;
+                });
+                cur = cur[cur.length - 1];
+                let id = cur && cur.length ? cur[0].id : "";
+    
+                if ( lastId !== id ) {
+                    lastId = id;
+                    id == "" && ( id = "sr-toc-0" );
+                    menuItems.parent().find( "active" ).removeClass( "toc-outline-active" );
+                    menuItems.filter("[href='#"+id+"']").parent().find( "active" ).addClass( "toc-outline-active" );
+                }
+            };
 
-        $( window ).scroll( function() {
-            if ( is_click ) return;
-            const fromTop = $(this).scrollTop() + topMenuHeight;
-            let cur = scrollItems.map( function() {
-            if ($(this).offset().top < fromTop)
-                return this;
-            });
-            cur = cur[cur.length - 1];
-            let id = cur && cur.length ? cur[0].id : "";
-
-            if ( lastId !== id ) {
-                lastId = id;
-                id == "" && ( id = "sr-toc-0" );
-                menuItems.parent().find( "active" ).removeClass( "toc-outline-active" );
-                menuItems.filter("[href='#"+id+"']").parent().find( "active" ).addClass( "toc-outline-active" );
-            }
-        });
+        $( window ).scroll( setActive );
 
         $( "outline" ).map( ( idx, item ) => {
             $(item).width( 180 - parseInt( $(item).css("padding-left") ) );
         })
 
         this.props.hidden == false && $( "toc" ).css({ "width" : "initial" });
+
+        setActive();
     }
 
     render() {
