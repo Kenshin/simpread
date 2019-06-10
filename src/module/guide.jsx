3c7fecd4ab82980262f50f463c8bc8dd68223696
Kenshin <fxblog@gmail.com>
2019-06-10 15:54:14 +0800

    Format source.

--- src/module/guide.jsx
+++ src/module/guide.jsx
@@ -61,7 +61,7 @@ export default class Guide extends React.Component {
         $( ".guide .loading" ).html( `<svg width="20" height="20" viewBox="0 0 38 38" stroke="#26d07c"> <g fill="none" fill-rule="evenodd"> <g transform="translate(1 1)" stroke-width="2"> <circle stroke-opacity=".5" cx="18" cy="18" r="18"/> <path d="M36 18c0-9.94-8.06-18-18-18"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/> </path> </g> </g></svg>` );
         const ajax = () => {
             $.ajax({
-                url   : storage.help_service,
+                url   : storage.help_service + "?=" + Math.round(+new Date()),
                 method: "GET",
             }).done( ( result, textStatus, jqXHR ) => {
                 if ( result && result.tips.length == 0 ) {
