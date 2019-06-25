f786639c53131c10e60970e8d0a31fb9c1ca4e39
Kenshin <fxblog@gmail.com>
2019-06-25 11:07:00 +0800

    Add ver.VerifyPlugin() to workflow.

--- src/options/options.js
+++ src/options/options.js
@@ -183,6 +183,10 @@ function vernotify( first ) {
                 });
             }
             ///////////////////////////////////////////////////////////////////////////
+            // verify and remove old plugins
+            ver.VerifyPlugin( storage.option, version ) && storage.Write( () => {
+                new Notify().Render({ content: `新版升级后，会自动删除一些已失效的插件，详细请看 <a href="http://ksria.com/simpread/welcome/version_${version}.html#badplugins" target="_blank">自定义主题</a>`, state: "holdon" });
+            }, storage.simpread );
         }
         website_sync = true;
         browser.runtime.sendMessage({ type: "track", value: { eventAction: hash.startsWith( "#firstload?ver=" ) ? "install" : "update" , eventCategory: "install", eventLabel: "install && update" } });
