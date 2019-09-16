89ad6b5dcc6acc8bfa82c980aa41cb0a03b5aba4
Kenshin <fxblog@gmail.com>
2019-09-16 10:36:02 +0800

    Add formData logic to corb.

--- src/options/corb.js
+++ src/options/corb.js
@@ -10,6 +10,11 @@ import * as msg   from 'message';
 browser.runtime.onMessage.addListener( function( request, sender, sendResponse ) {
     if ( request.type == msg.MESSAGE_ACTION.AXIOS ) {
         if ( request.value.type == "post" ) {
+            if ( request.value.form ) {
+                const formData = new FormData();
+                Object.keys( request.value.form ).forEach( key => formData.append( key, request.value.form[key] ) );
+                request.value.data = formData;
+            }
             axios.post( request.value.url, request.value.data )
                 .then( response => sendResponse({ done: response }))
                 .catch( error   => sendResponse({ fail: error    }));
