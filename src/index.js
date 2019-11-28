da5a995c326884f0dcdd04b796bebfe888d6832b
Kenshin <fxblog@gmail.com>
2019-11-28 18:49:54 +0800

    Optimize source.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import 'main';
 
 // main
 $( document ).ready( function() {
-    var swiper = new Swiper('.swiper-container', {
-      pagination : { el: '.swiper-pagination', loop: true, clickable: true,dynamicBullets: true}
-    });
+    analyticsRender();
+    reviewsRender();
 });
 
+function reviewsRender() {
+    const swiper = new Swiper('.swiper-container', {
+        pagination : { el: '.swiper-pagination', loop: true, clickable: true,dynamicBullets: true}
+    });
+}
+
 // google analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-405976-9');
+function analyticsRender() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-405976-9');
+}
