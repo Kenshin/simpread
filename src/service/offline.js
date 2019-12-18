fc7139f991f102821e47f7bbc235e0d6490bea5f
Kenshin <fxblog@gmail.com>
2019-12-18 11:07:56 +0800

    Add custom to offline html.

--- src/service/offline.js
+++ src/service/offline.js
@@ -16,7 +16,14 @@ let currIdx = 0, maxCount = 0, urls = [], images, cb;
  * @return {string} html
  */
 function HTML( title, desc, content, styles ) {
-    const   html = `
+    const custom = () => {
+            let styles = "";
+            $( "head" ).find( "style" ).map( (index, item) => {
+                item.id.startsWith( "simpread-custom-" ) && ( styles += item.innerHTML );
+            });
+            return styles;
+        },
+          html = `
                 <html lang="en" class="simpread-font simpread-theme-root" style='${ $( "html" ).attr( "style" ) }'>
                     <head>
                         <meta charset="UTF-8">
@@ -27,6 +34,7 @@ function HTML( title, desc, content, styles ) {
                         <style type="text/css">${ styles.common }</style>
                         <style type="text/css">${ styles.theme  }</style>
                         <style type="text/css">${ styles.global }</style>
+                        <style type="text/css">${ custom()      }</style>
                         <style type="text/css">${ styles.css    }</style>
                         <title>简悦 | ${title}</title>
                     </head>
