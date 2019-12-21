e1cfb1839b1a05da80bc7219bc646932dc8ac38c
Kenshin <fxblog@gmail.com>
2019-12-21 16:48:16 +0800

    Add weizhi auth logic.

--- src/module/authorize.jsx
+++ src/module/authorize.jsx
@@ -27,6 +27,10 @@ export default class Auth extends React.Component {
         jianguo: {
             username: "",
             password: "",
+        },
+        weizhi: {
+            username: "",
+            password: "",
         }
     }
 
@@ -35,13 +39,14 @@ export default class Auth extends React.Component {
         linnk  : undefined,
         instapaper : undefined,
         jianguo: undefined,
+        weizhi : undefined,
         notion : undefined,
         youdao : undefined,
     }
 
     onChange( state, value, flag ) {
         let notify;
-        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive, jianguo, yuque, notion, youdao } = exp,
+        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive, jianguo, yuque, notion, youdao, weizhi } = exp,
             clear = ( id, name ) => {
                 Object.keys( storage.secret[id] ).forEach( item => storage.secret[id][item] = "" );
                 storage.Safe( ()=> {
@@ -58,6 +63,7 @@ export default class Auth extends React.Component {
                     id == "linnk"      && this.setState({ secret: storage.secret, linnk: false });
                     id == "instapaper" && this.setState({ secret: storage.secret, instapaper: false });
                     id == "jianguo"    && this.setState({ secret: storage.secret, jianguo: false });
+                    id == "weizhi"     && this.setState({ secret: storage.secret, weizhi: false });
                     id == "notion"     && this.setState({ secret: storage.secret, notion: notion.blocks });
                     id == "youdao"     && this.setState({ secret: storage.secret, youdao: youdao.folders });
                     if ( location.hash.startsWith( "#labs?auth=" ) ) {
@@ -92,6 +98,11 @@ export default class Auth extends React.Component {
             return;
         }
 
+        if ( state == "weizhi" && !flag && !storage.secret.weizhi.username ) {
+            this.setState({ weizhi: !this.state.weizhi });
+            return;
+        }
+
         if ( !value ) {
             state == "pocket" && $( this.refs.pocket_tags ).velocity( value ? "slideDown" : "slideUp" );
             if ( state == "linnk" ) {
@@ -105,6 +116,10 @@ export default class Auth extends React.Component {
             if ( state == "jianguo" ) {
                 this.props.jianguo.username = "";
                 this.props.jianguo.password = "";
+            }
+            if ( state == "weizhi" ) {
+                this.props.weizhi.username = "";
+                this.props.weizhi.access_token = "";
             }
             if ( state == "youdao" ) {
                 permission.Remove( youdao.permissions, result => new Notify().Render( `已取消 cookies 权限。` ));
@@ -251,6 +266,13 @@ export default class Auth extends React.Component {
                     } else success( "jianguo", "坚果云", { username: this.props.jianguo.username, password: this.props.jianguo.password } );
                 });
                 break;
+            case "weizhi":
+                weizhi.Auth( this.props.weizhi.username, this.props.weizhi.password, result => {
+                    if ( result && result.status == 401 ) {
+                        failed( "授权错误，请重新授权。", weizhi.id, weizhi.name );
+                    } else success( "weizhi", "为知笔记", { username: this.props.weizhi.username, access_token: weizhi.access_token } );
+                });
+                break;
         }
     }
 
@@ -272,6 +294,10 @@ export default class Auth extends React.Component {
 
     jianguoOnChange( state, value ) {
         this.props.jianguo[state] = value;
+    }
+
+    weizhiOnChange( state, value ) {
+        this.props.weizhi[state] = value;
     }
 
     webdavOnChange() {
@@ -504,6 +530,34 @@ export default class Auth extends React.Component {
                                     onClick={ (s)=>this.youdaoChange() } /> }
                             </div> }
 
+                            <div className="version-tips" data-version="1.1.3" data-hits="weizhi">
+                            <Switch width="100%" checked={ this.state.secret.weizhi && this.state.secret.weizhi.username != "" && this.state.secret.weizhi.access_token ? true : false }
+                                thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
+                                label={ this.state.secret.weizhi && this.state.secret.weizhi.username != "" ? "已授权 为知笔记，是否取消授权？" : "是否连接并授权 为知笔记 ？" }
+                                onChange={ (s)=>this.onChange( "weizhi", s ) } />
+                            </div>
+                            { this.state.weizhi && 
+                            <div ref="weizhi">
+                                <div style={{ "display": "flex", "flex-direction": "row" }}>
+                                    <TextField
+                                        placeholder="请填入 为知笔记 的用户名，简悦不会记录你的用户名。" 
+                                        onChange={ (evt)=>this.weizhiOnChange( "username", evt.target.value ) }
+                                    />
+                                    <TextField
+                                        password={true}
+                                        placeholder="请填入 为知笔记 的密码，简悦不会记录你的密码。" 
+                                        onChange={ (evt)=>this.weizhiOnChange( "password", evt.target.value ) }
+                                    />
+                                </div>
+    
+                                <Button type="raised" width="100%" style={{ "margin": "0" }}
+                                    text="绑定 为知笔记 的信息"
+                                    color="#fff" backgroundColor="#3F51B5"
+                                    waves="md-waves-effect md-waves-button"
+                                    onClick={ (s)=>this.onChange( "weizhi", s, "login" ) } />
+    
+                            </div> }
+    
                         <div className="version-tips" data-version="1.1.3" data-hits="webdav">
                         <div className="label" style={{'margin-bottom':' -15px'}}>WebDAV</div>
                         <div className="sublabel">简悦支持任意 WebDAV 的服务，包括：Box · TeraCLOUD 等</div>
