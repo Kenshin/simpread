b6fd716c274fb40098fbe1e814a76b490eb6da98
Kenshin <fxblog@gmail.com>
2020-01-08 13:13:37 +0800

    Add ga.js to all page.

---
+++ src/ga.js
@@ -0,0 +1,22 @@
+
+/**
+ * Track using the asynchronous tracking API.
+ *
+ * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
+ * for information on how to use the asynchronous tracking API.
+ * _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+ */
+
+var _AnalyticsCode = 'UA-405976-14';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
