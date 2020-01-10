a1228545ee74bdbdc163724553dee12a70940679
Kenshin <fxblog@gmail.com>
2020-01-10 12:58:16 +0800

    Add th.GetAll().

--- src/service/theme.js
+++ src/service/theme.js
@@ -70,6 +70,10 @@ class Theme {
         return themes[theme];
     }
 
+    GetAll() {
+        findThemeStyle();
+    }
+
     constructor() {
         require( `../assets/css/theme_common.css` );
         names.forEach( name => require( `../assets/css/theme_${name}.css` ) );
@@ -90,13 +94,13 @@ function findThemeStyle( callback ) {
         if ( css.startsWith( flag ) ) {
             const arr  = css.replace( flag, "" ).match( /\w+/ ),
                   name = arr[ arr.length - 1 ];
-            callback( name, css, $target );
+            callback && callback( name, css, $target );
         } else if ( css.search( ".simpread-font" ) > -1 ) {
-            themes["global"] = css;
+            !themes["global"] && ( themes["global"] = css );
         } else if ( css.search( "(pointer: coarse)" ) == -1 && css.search( ".simpread-theme-root" ) > -1 ) {
-            themes["common"] = css;
+            !themes["common"] && ( themes["common"] = css );
         } else if ( css.search( "(pointer: coarse)" ) > -1 && css.search( "sr-read" ) > -1 ) {
-            themes["mobile"] = css;
+            !themes["mobile"] && ( themes["mobile"] = css );
         }
     });
 }
