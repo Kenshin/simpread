75b70c8d726b09b578a00f4345c7f61fc4600504
Kenshin <fxblog@gmail.com>
2020-01-10 14:25:20 +0800

    Fix focus mode controlbar some bug.

--- src/focus/controlbar.jsx
+++ src/focus/controlbar.jsx
@@ -33,6 +33,13 @@ class FControl extends React.Component {
         });
     }
 
+    onClose() {
+        setTimeout( ()=> {
+            $( "fab-bg" ).css({ width: '100px' })
+            $( "fab" ).find( "ul[type=hori]" ).css({ opacity: 0, visibility: "hidden" })
+        }, 200 );
+    }
+
     onAction( event, type ) {
         console.log( "fab type is =", type )
 
@@ -46,9 +53,11 @@ class FControl extends React.Component {
                     break;
                 case "setting":
                     setting.Render( ()=>setTimeout( ()=>se.Render(), 500 ));
+                    this.onClose();
                     break;
                 case "siteeditor":
                     se.Render();
+                    this.onClose();
                     break;
                 case "remove":
                     new Notify().Render( "移动鼠标选择不想显示的内容，只针对本次有效。" );
